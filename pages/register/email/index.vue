5a189dea9e9fd4edc57cd0bf1c7ef0d541d015b3
由解　仁知翔 <e195765@ie.u-ryukyu.ac.jp>
2021-06-30 15:55:21 +0900

    fix email-register-page

--- pages/register/email/index.vue
+++ pages/register/email/index.vue
@@ -29,19 +29,22 @@
       <p>
         確認コードを入力
       </p>
-      <el-input placeholder="確認コードを入力" v-model="code"></el-input>
-<br><br>
+      <el-input placeholder="確認コードを入力" v-model="code" @input="checkPass"></el-input>
+<br><br><br><br>
 <!--確認コードが一致したら表示する-->
+  <transition name="fade">
+    <div v-show="show">
       <el-steps :active="3">
         <el-step title="Step 1" description="情報入力"></el-step>
         <el-step title="Step 2" description="認証"></el-step>
         <el-step title="Step 3" description="登録完了！"></el-step>
       </el-steps>
-<br>
+<br><br>
       <p style="text-align:center;">
         登録完了しました！
       </p>
-
+    </div> 
+  </transition>
   </div>
 </template>
 
@@ -61,10 +64,22 @@ export default {
       }
   },
   methods: {
-    
+    checkPass: function() {
+      this.show = this.code === "1234" /*とりあえず1234てテストしてます，後日正しく改善*/
+    }
   },
   created() {
     
   },
 }
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  will-change: opacity;
+  transition: opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
